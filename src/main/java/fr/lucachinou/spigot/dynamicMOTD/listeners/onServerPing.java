222a21e0788016ca04d1b2bf8ce3ec512292211f
lucachinou <lucachinou@duck.com>
2025-09-11 12:53:39 +0200

    Fixed the reference to Bungeecord side into Spigot side

--- src/main/java/fr/lucachinou/spigot/dynamicMOTD/listeners/onServerPing.java
+++ src/main/java/fr/lucachinou/spigot/dynamicMOTD/listeners/onServerPing.java
@@ -1,6 +1,6 @@
 package fr.lucachinou.spigot.dynamicMOTD.listeners;
 
-import fr.lucachinou.Bungeecord.dynamicMOTD.DynamicMOTD;
+import fr.lucachinou.spigot.dynamicMOTD.DynamicMOTD;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
